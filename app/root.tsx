9a0d506d1ae81fa84fbdfc8dfe51f6d4ed3bdf50
Putnug1122 <asiapatta10@gmail.com>
2023-04-17 06:52:35 +0800

    catch boundary, show dynamic content, and dynamic meta

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import styles from "~/styles/main.css";
 import MainNavigation from "~/components/MainNavigation";
@@ -24,6 +25,38 @@ export default function App() {
           <MainNavigation />
         </header>
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caughtResponse = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>{caughtResponse.statusText}</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className="error">
+          <h1>{caughtResponse.statusText}</h1>
+          <p>
+            <pre>{caughtResponse.data?.message || "Something went wrong!"}</pre>
+          </p>
+          <p>
+            <Link to="/">Go home</Link>
+          </p>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/notes.$id.tsx
+++ app/routes/notes.$id.tsx
@@ -1,18 +1,36 @@
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, type V2_MetaFunction, useLoaderData } from "@remix-run/react";
+import { getStoredNotes } from "~/data/notes";
 import style from "~/styles/note-details.css";
 
+export const meta: V2_MetaFunction = ({ data }) => {
+  return [{ title: data.title }];
+};
+
 export default function NoteDetailPage() {
+  const note = useLoaderData();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all Notes</Link>
         </nav>
-        <h1>Note TITLE</h1>
-        <p id="note-details-content">NOTE CONTENT</p>
+        <h1>{note.title}</h1>
+        <p id="note-details-content">{note.content}</p>
       </header>
     </main>
   );
+}
+
+export async function loader({ params }: { params: { id: string } }) {
+  const notes = await getStoredNotes();
+  const selectedNote = notes.find((note) => note.id === params.id);
+
+  if (!selectedNote) {
+    throw json({ message: "Note not found" }, { status: 404 });
+  }
+  return selectedNote;
 }
 
 export function links() {
